a4be6cb377e0ef717acdd1464191338bf43a289c
VikaLevchyshyna <vika.levchyshyna@gmail.com>
2021-08-30 16:22:14 +0300

    hw2 done

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+import data from './../data/data';
+
+class App extends Component {
+  state = data;
+
+  onLeaveFeedback = option => {
+    this.setState(state => ({
+      [option]: state[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, point) => acc + point, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const condition = Math.round(
+      (this.state.good / this.countTotalFeedback()) * 100,
+    );
+    return condition > 0 ? condition : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positiveFeedback={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message={'No feedback given'} />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
